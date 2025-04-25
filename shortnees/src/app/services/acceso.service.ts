716db4d68169f4eacdc776810d02a6f4e5b77664
Luis Torices <luis.babuska@gmail.com>
2025-04-25 14:10:09 +0200

    preparativos despliegue

--- shortnees/src/app/services/acceso.service.ts
+++ shortnees/src/app/services/acceso.service.ts
@@ -3,48 +3,57 @@ import { ResponseAcceso } from '../interfaces/ResponseAcceso';
 import { HttpClient } from '@angular/common/http';
 import { appsettings } from '../settings/appsettings';
 import { Usuario } from '../interfaces/Usuario';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { Login } from '../interfaces/Login';
 import { ResolverTokenService } from './resolver-token.service';
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class AccesoService {
   private http = inject(HttpClient);
-  private resolverToken = inject(ResolverTokenService);
   private baseUrl: string = appsettings.apiUrl;
-  private isLoggedIn = signal(false);
-  private userName = signal<string>('');
-  constructor() { 
-    this.checkInitialAuthState();
-    const storedUserName = localStorage.getItem('email');
-    this.userName.set(storedUserName || '');
+  // BehaviorSubject para mantener y compartir el estado de autenticación
+  private isAuthenticatedSubject = new BehaviorSubject<boolean>(this.hasToken());
+  // Observable público que los componentes pueden suscribir
+  public isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
+  constructor(
+    private router: Router,
+    private resolverToken: ResolverTokenService
+  ) { }
+  // Verifica si hay un token almacenado
+  private hasToken(): boolean {
+    return !!localStorage.getItem('token');
   }
   registrarse(objeto:Usuario): Observable<any>{
     return this.http.post<any>(`${this.baseUrl}registro`,objeto)
   }
   login(objeto:Login): Observable<ResponseAcceso>{
     //this.userName.set(objeto.username);
+    return this.http.post<ResponseAcceso>(`${this.baseUrl}api/login_check`,objeto).pipe(
+      map((res: any) => {
+        this.authSuccess(res.access_token);
+        return res;
+      })
+    );
+  }
+  
+  // Método llamado al autenticar exitosamente
+  authSuccess(token: string) {
+    localStorage.setItem('token', token);
+    this.resolverToken.getUsername()
     
-    return this.http.post<ResponseAcceso>(`${this.baseUrl}api/login_check`,objeto)
-  }
-  get loginStatus() {
-    return this.isLoggedIn.asReadonly();
-  }
-  get userNames() {
-    return this.userName.asReadonly();
-  }
-  private checkInitialAuthState() {
-    // Lógica para verificar si el usuario ya está autenticado
-    const token = localStorage.getItem('token');
-    if (token) {
-      this.isLoggedIn.set(true);
-    }
+    /*
+    ocuparse aqui de nombre de usuario y opciones
+    
+    */ 
+    // Actualiza el BehaviorSubject a true
+    this.isAuthenticatedSubject.next(true);
   }
   logout(): void {
     localStorage.removeItem("token"); // Elimina el token del localStorage
     localStorage.removeItem('email');
     localStorage.removeItem('rol');
-    this.isLoggedIn.set(false);
+    this.router.navigate(['login']);
   }
 }
