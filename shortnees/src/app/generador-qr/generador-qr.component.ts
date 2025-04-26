e77d2152387ef6fa6f4625205cc39933b3452be5
Luis Torices <luis.babuska@gmail.com>
2025-04-26 21:02:54 +0200

    integracion de angularx-qrcode 18.0.2

---
+++ shortnees/src/app/generador-qr/generador-qr.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generador-qr',
+  standalone: true,
+  imports: [],
+  templateUrl: './generador-qr.component.html',
+  styleUrl: './generador-qr.component.css'
+})
+export class GeneradorQRComponent {
+
+}
