5fd6dceca8523b16cdf74d64ea892fb2c1127d53
Luis Torices <luis.babuska@gmail.com>
2026-05-06 01:36:56 +0200

    reestructuracion, home

--- shortnees/src/app/home/home.component.ts
+++ shortnees/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LinkService } from '../services/link.service';
 import { LinkResponse } from '../interfaces/link-response';
+import { AccesoService } from '../services/acceso.service';
 
 @Component({
   selector: 'app-home',
@@ -11,18 +12,50 @@ import { LinkResponse } from '../interfaces/link-response';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  shortUrl: string = '';
+  error: string = '';
+  formulario: FormGroup;
+
   shortUrlP: string = '';
   errorP: string = '';
   formularioPersonalizar: FormGroup;
 
+  isAuthenticated: boolean = false;
+
   constructor(
     private linkService: LinkService,
     private fb: FormBuilder,
+    private accesoService: AccesoService,
   ) {
+    this.formulario = this.fb.group({
+      url: ['', Validators.required]
+    });
+
     this.formularioPersonalizar = this.fb.group({
       urlOriginal: ['', Validators.required],
       urlPersonalizada: ['', Validators.required]
     });
+
+    this.accesoService.isAuthenticated$.subscribe(val => this.isAuthenticated = val);
+  }
+
+  acortar() {
+    if (this.formulario.valid) {
+      this.linkService.enviarLink(this.formulario.value).subscribe({
+        next: (data: LinkResponse) => {
+          if (data.urlCorta === 'protocolo') {
+            this.error = data.mensaje;
+            this.shortUrl = '';
+          } else if (data.mensaje === 'Enlace creado') {
+            this.shortUrl = data.urlCorta;
+            this.error = '';
+          } else {
+            this.error = data.mensaje;
+            this.shortUrl = '';
+          }
+        }
+      });
+    }
   }
 
   personalizar() {
