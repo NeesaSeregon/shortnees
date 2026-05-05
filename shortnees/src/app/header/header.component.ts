0d5bf91ebf989c60cdc28b5cd3185b3b5ff4f89b
Luis Torices <luis.babuska@gmail.com>
2026-05-05 16:11:03 +0200

    optimizacion y limpieza

--- shortnees/src/app/header/header.component.ts
+++ shortnees/src/app/header/header.component.ts
@@ -1,26 +1,24 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccesoService } from '../services/acceso.service';
 import { Router } from '@angular/router';
 import { inject } from '@angular/core';
-import { ResolverTokenService } from '../services/resolver-token.service'
-import { Subscription } from 'rxjs';import { Usuario } from '../interfaces/Usuario';
-import { NgIf } from '@angular/common';
+import { Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, NgIf,MatListModule,MatIconModule],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit {
   private router = inject(Router);
-  private resolverToken = inject(ResolverTokenService);
-  public isLoggedIn : any = false;
+  public isLoggedIn: any = false;
   private authSubscription: Subscription | undefined;
+
   constructor(public accesoService: AccesoService) {}
+
   ngOnInit() {
     this.authSubscription = this.accesoService.isAuthenticated$.subscribe(
       (isAuthenticated) => {
@@ -28,12 +26,13 @@ export class HeaderComponent implements OnInit {
       }
     );
   }
+
   logout() {
     this.accesoService.logout();
     this.router.navigate(['/login']);
   }
+
   ngOnDestroy(): void {
-    // Importante: cancelar la suscripción para evitar memory leaks
     if (this.authSubscription) {
       this.authSubscription.unsubscribe();
     }
--- shortnees/src/app/home/home.component.ts
+++ shortnees/src/app/home/home.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ResolverTokenService } from '../services/resolver-token.service';
-import { Router, RouterOutlet } from '@angular/router';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { LinkService } from '../services/link.service';
-import { Url } from '../interfaces/Url';
-import { AccesoService } from '../services/acceso.service';
 import { LinkResponse } from '../interfaces/link-response';
-import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -15,84 +11,34 @@ import { Subscription } from 'rxjs';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  shortUrl: string = '';
-  error: string = '';
   shortUrlP: string = '';
   errorP: string = '';
-  isLoggedIn:any;
   formularioPersonalizar: FormGroup;
-  formularioSinRegistro: FormGroup;
-  user2: any;private authSubscription: Subscription | undefined;
+
   constructor(
-    private resolverToken: ResolverTokenService,
-    private linkService : LinkService,
-    private fb : FormBuilder,
-    private accesoService: AccesoService,
-    private router: Router
+    private linkService: LinkService,
+    private fb: FormBuilder,
   ) {
     this.formularioPersonalizar = this.fb.group({
       urlOriginal: [''],
       urlPersonalizada: ['']
     });
-    this.formularioSinRegistro = this.fb.group({
-      urlOriginal: [''],
-    });
   }
-  ngOnInit() {
-    const user = this.resolverToken.getUser();
-    this.authSubscription = this.accesoService.isAuthenticated$.subscribe(
-      (isAuthenticated) => {
-        this.isLoggedIn = isAuthenticated;
-      }
-    );
-  }
-  acortarHash() {
-    let url:Url = {
-      url: this.formularioSinRegistro.value['urlOriginal']
-    };
-    if(this.formularioSinRegistro.value['urlOriginal'] != null){
-      this.linkService.enviarLink(url).subscribe({
-        next: (data:LinkResponse) => {
-          if(data.urlCorta==='protocolo'){
-            //si el protocolo no es https
-            this.error=data.mensaje;
-          }else if (data.mensaje === 'Enlace creado'){
-            //si el enlace se acorto correctamente
-            this.shortUrl=data.urlCorta;
-            this.error = data.mensaje
-          }else if (data.mensaje === 'No puede dejar su URL en blanco') {
-            this.error = data.mensaje
-          }else{
-            this.error = 'error desconocido'
-          }
-          //LLAMAR EVENTO: MOSTRAR LA URL 
-        }
-      })
-    }
-  }
+
   personalizar() {
     if (this.formularioPersonalizar.valid) {
-      console.log(this.formularioPersonalizar.value);
       this.linkService.personalizarLink(this.formularioPersonalizar.value).subscribe({
-        next: (data:LinkResponse) => {
-          if(data.urlCorta==='protocolo'){
-            //si el protocolo no es https
-            this.errorP=data.mensaje;
-          }else if (data.mensaje === 'Enlace creado'){
-            //si el enlace se acorto correctamente
-            this.shortUrlP=data.urlCorta;
-            this.errorP = data.mensaje
-          }else{
-            this.errorP = data.mensaje
+        next: (data: LinkResponse) => {
+          if (data.urlCorta === 'protocolo') {
+            this.errorP = data.mensaje;
+          } else if (data.mensaje === 'Enlace creado') {
+            this.shortUrlP = data.urlCorta;
+            this.errorP = data.mensaje;
+          } else {
+            this.errorP = data.mensaje;
           }
         }
       });
-    }
-  }
-  ngOnDestroy(): void {
-    // Importante: cancelar la suscripción para evitar memory leaks
-    if (this.authSubscription) {
-      this.authSubscription.unsubscribe();
     }
   }
 }
