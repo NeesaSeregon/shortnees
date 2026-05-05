59e826a184a8577361c3c53aa3230baba24fc93c
Luis Torices <luis.babuska@gmail.com>
2026-05-05 16:50:24 +0200

    icono qr

--- shortnees/src/app/generador-qr/generador-qr.component.ts
+++ shortnees/src/app/generador-qr/generador-qr.component.ts
@@ -13,77 +13,53 @@ export class GeneradorQRComponent implements OnInit{
   qrCode: QRCodeStyling = new QRCodeStyling;
   constructor(){}
   @ViewChild('canvas', { static: true }) canvas!: ElementRef;
-
   ngOnInit(): void {
     this.qrCode = new QRCodeStyling({
       width: 300,
       height: 300,
       type: 'svg',
       data: this.data,
-      image: '/LogotipoShortneesClaro.png',
+      //image: '/LogotipoShortneesClaro.png',
       margin: 10,
       qrOptions: {
         typeNumber: 0,
         mode: 'Byte',
         errorCorrectionLevel: 'Q'
       },
-      imageOptions: {
+      /*imageOptions: {
         hideBackgroundDots: true,
         imageSize: 0.4,
         margin: 20,
         crossOrigin: 'anonymous',
-      },
+      },*/
       dotsOptions: {
         color: '#BD022D',
-        // gradient: {
-        //   type: 'linear', // 'radial'
-        //   rotation: 0,
-        //   colorStops: [{ offset: 0, color: '#8688B2' }, { offset: 1, color: '#77779C' }]
-        // },
         type: 'rounded'
       },
       backgroundOptions: {
         color: '#e9ebee',
-        // gradient: {
-        //   type: 'linear', // 'radial'
-        //   rotation: 0,
-        //   colorStops: [{ offset: 0, color: '#ededff' }, { offset: 1, color: '#e6e7ff' }]
-        // },
       },
       cornersSquareOptions: {
         color: '#BD022D',
         type: 'extra-rounded',
-        // gradient: {
-        //   type: 'linear', // 'radial'
-        //   rotation: 180,
-        //   colorStops: [{ offset: 0, color: '#25456e' }, { offset: 1, color: '#4267b2' }]
-        // },
       },
       cornersDotOptions: {
         color: '#BD022D',
         type: 'dot',
-        // gradient: {
-        //   type: 'linear', // 'radial'
-        //   rotation: 180,
-        //   colorStops: [{ offset: 0, color: '#00266e' }, { offset: 1, color: '#4060b3' }]
-        // },
       }
     });
 
     this.qrCode.append(this.canvas.nativeElement);
   }
-
   onKey(event: any): void {
     this.data = event.target.value;
     this.qrCode.update({
       data: this.data
     });
   }
-
   onChange(event: any): void {
     this.extension = event.target.value;
   }
-
   download(): void {
     this.qrCode.download({ extension: this.extension as FileExtension});
   }
